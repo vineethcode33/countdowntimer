e7aa1a013b8c7e1482094dbdb962df0bbbce67da
vineethcode33 <vineethcode@outlook.com>
2021-05-03 19:01:33 -0700

    feat(countdown): added timer functionality

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import "./App.css"
 import { v4 as uuidv4 } from 'uuid';
-const { useState } = React;
+const { useState, useEffect } = React;
 
 const CountDownTimers = ({ allTimers }) => {
   return (
@@ -10,7 +10,7 @@ const CountDownTimers = ({ allTimers }) => {
         allTimers.map((timer, index) => (
           <div key={uuidv4()}>
             <p>Name: {timer.timerName}</p>
-            <p>Count down: {`${timer.days}:${timer.hours}:${timer.minutes}:${timer.seconds}`}</p>
+            <p>Count down: {`${timer.days}D:${timer.hours}H:${timer.minutes}M:${timer.seconds}S`}</p>
           </div>
         ))
      }
@@ -26,22 +26,54 @@ const TimerInput = () => {
   const [allTimers, setAllTimers] = useState([])
 
 
-  const handleSubmit = (e) => {
+  const calTime = (userDate) => {
+    let dateNow = new Date()
 
+    let elapsed_seconds = (userDate - dateNow)/1000
+    let days = Math.floor(elapsed_seconds / (3600*24))
+    let hours = Math.floor((elapsed_seconds % (3600*24)) / (3600))
+    let minutes = Math.floor((elapsed_seconds % (3600))/(60))
+    let seconds = Math.floor(elapsed_seconds % 60)
+    return {days, hours, minutes, seconds}
+  }
+
+  const updateCountDownTimer = (timers) => {
+    let newTimers=[];
+    timers.forEach((timer, index) => {
+      let userDate = timer.dateTime
+      let { days, hours, minutes, seconds } = calTime(userDate);
+
+      newTimers.push({
+      days, hours, minutes, seconds,
+      timerName: timer.timerName,
+      dateTime: timer.dateTime,
+      id: timer.id
+      })
+    })
+    return newTimers;
+  }
+
+   useEffect(() => {
+    const interval = setInterval(() => {
+      if (allTimers.length > 0) {
+        let newTimers = updateCountDownTimer(allTimers)
+        setAllTimers(newTimers)
+      }
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [updateCountDownTimer,allTimers]);
+
+
+
+  const handleSubmit = (e) => {
     e.preventDefault()
     let existingTimers = [...allTimers]
-    let dateNow = Date.now()
-    let userDate = new Date(timerDate + ' ' + timerTime)
-    console.log(userDate.toUTCMilliseconds())
-    let elapsed =  userDate.getUTCMilliseconds() - dateNow
-    let days = Math.floor(elapsed / (3600*24))
-    let hours = Math.floor((elapsed % (3600*24)) / 3600)
-    let minutes = Math.floor((elapsed % 3600)/60)
-    let seconds = Math.floor(elapsed % 60)
+    let userDate = new Date(timerDate.toString() + ' ' + timerTime.toString())
+    let { days, hours, minutes, seconds } = calTime(userDate)
 
     existingTimers.push({
       timerName,
-      dateTime: new Date(timerDate + ' ' + timerTime),
+      dateTime: new Date(timerDate.toString() + ' ' + timerTime.toString()),
       id: uuidv4(),
       days,
       hours,
