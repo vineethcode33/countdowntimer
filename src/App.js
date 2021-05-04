65c06ec838d65170b19c3fd69e5081b607b3354d
vineethcode33 <vineethcode@outlook.com>
2021-05-03 18:02:28 -0700

    feat(countdown): added timer and setup

--- src/App.js
+++ src/App.js
@@ -3,6 +3,21 @@ import "./App.css"
 import { v4 as uuidv4 } from 'uuid';
 const { useState } = React;
 
+const CountDownTimers = ({ allTimers }) => {
+  return (
+    <div>
+      {
+        allTimers.map((timer, index) => (
+          <div key={uuidv4()}>
+            <p>Name: {timer.timerName}</p>
+            <p>Count down: {`${timer.days}:${timer.hours}:${timer.minutes}:${timer.seconds}`}</p>
+          </div>
+        ))
+     }
+    </div>
+  )
+}
+
 const TimerInput = () => {
 
   const [timerName, setTimerName] = useState("")
@@ -12,22 +27,32 @@ const TimerInput = () => {
 
 
   const handleSubmit = (e) => {
-    console.log("called on submit")
-    let existingTimers = [...allTimers]
-    existingTimers.push({
-      timerName, dateTime: new Date(timerDate + ' ' + timerTime), id: uuidv4()
-    })
-    console.log("existingTimers: ", existingTimers);
-    setAllTimers(existingTimers)
+
     e.preventDefault()
+    let existingTimers = [...allTimers]
+    let dateNow = Date.now()
+    let userDate = new Date(timerDate + ' ' + timerTime)
+    console.log(userDate.toUTCMilliseconds())
+    let elapsed =  userDate.getUTCMilliseconds() - dateNow
+    let days = Math.floor(elapsed / (3600*24))
+    let hours = Math.floor((elapsed % (3600*24)) / 3600)
+    let minutes = Math.floor((elapsed % 3600)/60)
+    let seconds = Math.floor(elapsed % 60)
 
+    existingTimers.push({
+      timerName,
+      dateTime: new Date(timerDate + ' ' + timerTime),
+      id: uuidv4(),
+      days,
+      hours,
+      minutes,
+      seconds
+    })
+    setAllTimers(existingTimers)
+    setTimerName("")
+    setTimerDate("")
+    setTimerTime("")
   }
-
-  console.log("timerName:", timerName);
-  console.log("timerDate:", timerDate);
-  console.log("timerTime:", timerTime);
-  console.log("allTimers:", allTimers);
-
 
   return (
     <div className="CounterInputCard">
@@ -39,39 +64,18 @@ const TimerInput = () => {
         <label htmlFor="timerTime">Time:</label>
         <input value={timerTime} type="time" name="timerTime" id="timerTime" onChange={ (e) => setTimerTime(e.target.value)}/>
         <input type="submit" value='Start'/>
-        </form>
+      </form>
+      <CountDownTimers allTimers={allTimers}/>
     </div>
   )
 }
-
-const CountDownTimers = (allTimers) => {
-
-  return (
-    <div>
-      {
-        allTimers.map((timer) =>
-          <div>
-            <p>{timer.name}</p>
-            <p>{ timer.dateTime}</p>
-          </div>
-
-        )
-      }
-    </div>
-  )
-}
-
 
 function App() {
   return (
     <div className="App">
-      <TimerInput ></TimerInput>
-      {/* <CountDownTimers {...allTimers}/> */}
+      <TimerInput></TimerInput>
     </div>
   );
 }
-
-
-
 
 export default App;
